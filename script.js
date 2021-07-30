dedd90898a9c8612a2a4076e80a7a8c513ee436f
eenattayani <enryfirmana@gmail.com>
2021-07-30 10:06:14 +0700

    qr-code generator  oke

---
+++ script.js
@@ -0,0 +1,9 @@
+const qrcodeMin = require("./qrcode.min");
+
+function genQR(str) {
+  var str = document.querySelector("input").value;
+  QRCode.toCanvas(document.getElementById("canvas"), str, function (error) {
+    if (error) console.error(error);
+    console.log("success");
+  });
+}
